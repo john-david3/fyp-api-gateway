4b49d616ceaa8a1b20c5a3d49d1144f3cbd758fe
Ubuntu <122345776@umail.ucc.ie>
2026-03-26 12:26:23 +0000

    fix

--- dataplane/main.go
+++ dataplane/main.go
@@ -75,6 +75,7 @@ func handleNewConfig(w http.ResponseWriter, r *http.Request) {
 }
 
 func applyNginxConfig() error {
+	slog.Info("attempting to reload nginx config")
 	cmd := exec.Command("nginx", "-t")
 
 	output, err := cmd.CombinedOutput()
