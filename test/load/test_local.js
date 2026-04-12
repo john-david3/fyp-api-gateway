2cf84b7f3ee5b34574056ead9eba6b52e04bc505
John David White <127981157+john-david3@users.noreply.github.com>
2026-04-12 15:52:12 +0100

    Cleanup (#27)

--- test/load/test_local.js
+++ test/load/test_local.js
@@ -2,15 +2,17 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 50,
+    vus: 10,
     duration: '30s'
 };
 
 export default function () {
-    const res = http.get('http://localhost:8080/zutto_products');
+    const res = http.get('http://localhost:8080/test1_testhttp');
 
     check(res, {
         'status is 200': (r) => r.status === 200,
         'response time < 100ms': (r) => r.timings.duration < 100,
     });
+
+    sleep(1)
 }
